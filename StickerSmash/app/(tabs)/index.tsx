39f137aaafaa0cb15fd5b985bf4694702044c86f
Vitor-Ortiz <vitorortizoficial@gmail.com>
2025-02-26 10:51:38 -0300

    navegação por abas

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,14 +1,18 @@
+import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home.</Text>
+      <Link href="/about" style={styles.button}>
+        Sobre
+      </Link>
     </View>
   );
 }
 
-const syles = StyleSheet.create({
+const styles = StyleSheet.create({
   container:{
     flex: 1,
     backgroundColor: '#25292e',
@@ -16,6 +20,11 @@ const syles = StyleSheet.create({
     alignItems: "center",
   },
   text: {
+    color: '#fff'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
     color: '#fff'
   }
 })
